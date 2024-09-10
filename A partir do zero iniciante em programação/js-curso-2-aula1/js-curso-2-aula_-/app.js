45278a8f364f5fca985543b20bcc34598986f4a2
Alexandre S. Santos <alexandre-ssantos@outlook.com>
2024-09-10 01:11:06 -0300

    novos estudos

---
+++ A partir do zero iniciante em programação/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolhe um número entre 1 e 10';
+
+function verificarChute() {
+    
+}
