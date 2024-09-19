a1d508d73b6644043161f682a7011454e1489160
Alexandre S. Santos <alexandre-ssantos@outlook.com>
2024-09-18 23:49:37 -0300

    nova aula

--- A partir do zero iniciante em programação/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ A partir do zero iniciante em programação/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,16 +1,29 @@
+let numeroSecreto = numeroRandom();
+console.log(numeroSecreto);
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    console.log('O botão foi clicado');
+}
+
+function numeroRandom(){
+    return parseInt(Math.random() * 10 + 1);
+}
+
+
+
+
 // let titulo = document.querySelector('h1');
-// titulo.innerHTML = 'Jogo do número Secreto';
-
-// let paragrafo = document.querySelector('p');
-// paragrafo.innerHTML = 'Escolhe um número entre 1 e 10';
-
-// function verificarChute() {
-    
-// }
-
-
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Hora do Desafio';
+// titulo.innerHTML = 'Hora do Desafio';
 
 // function verificarChute () {
 //     console.log('O botão foi clicado');
@@ -25,9 +38,9 @@ titulo.innerHTML = 'Hora do Desafio';
 //     alert(`Estive em ${nomeCidade} e lembrei de você.`)
 // }
 
-function verificarChute(){
-    let numero1 = parseInt(prompt('Digite o primeiro número inteiro:'));
-    let numero2 = parseInt(prompt('Digite o segundo número inteiro:'));
-    let resultado = numero1 + numero2;
-    alert(`Este foi o resultado da soma dos números ${resultado}`)
-}
+// function verificarChute(){
+//     let numero1 = parseInt(prompt('Digite o primeiro número inteiro:'));
+//     let numero2 = parseInt(prompt('Digite o segundo número inteiro:'));
+//     let resultado = numero1 + numero2;
+//     alert(`Este foi o resultado da soma dos números ${resultado}`)
+// }
