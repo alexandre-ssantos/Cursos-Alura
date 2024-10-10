e5aea398fbe20f09a747f0446954dca96f258118
Alexandre S. Santos <alexandre-ssantos@outlook.com>
2024-10-09 23:40:30 -0300

    novas aulas

---
+++ A partir do zero iniciante em programação/js-curso-2-aula_4/exerc.js
@@ -0,0 +1,13 @@
+// let listaGenerica = [];
+// console.log(listaGenerica);
+
+// let linguagemDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+// console.log(linguagemDeProgramacao);
+
+// linguagemDeProgramacao.push("Java", "Ruby", "GoLang");
+// console.log(linguagemDeProgramacao);
+
+// let listaRandom = ["Skyline", "Jesko", "Regera"];
+// console.log(listaRandom [0]);
+// console.log(listaRandom [1]);
+// console.log(listaRandom [2]);
